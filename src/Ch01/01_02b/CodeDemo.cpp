ef192edbeed21ea4d726733e3329443245f6786d
TheCrickMaster <dakasture11@gmail.com>
2025-01-15 21:17:40 -0600

    Completed Ch1

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream> // Needed from the standard library
+
+int main(){ // The name of the function
+    std::cout << "Hello World!" << std::endl; // "Character Output Stream" followed by "Hello World!" and a new line
+
+    std::cout << std::endl << std::endl; // Two new lines for clarity
+    return (0); // Returning 0 to the console
+}
+// Sample Hello World program
--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -3,13 +3,17 @@
 // Console Interaction, by Eduardo Corpeño 
 
 #include <iostream>
-#include <string>
+#include <string> // Including string header file
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string str; // String
+
+    std::cout << "Please provide your name: " << std::flush; // "Ask person for their name
+    std::cin >> str; // "Character In"
+    std::cout << "Hello, " << str; // "Character Out"
 
     std::cout << std::endl << std::endl;
     return (0);
 }
+
+// The code above is a simple program that asks the user for their name and then greets them 
